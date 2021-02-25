069a4e2809b4e42fe2bfcd4eba502a73aa0b8c15
hugo bedford <hugo.bedford@gmail.com>
2021-02-25 09:20:24 -0300

    move LoginButton to its on file

---
+++ lib/ui/pages/login/components/login_button.dart
@@ -0,0 +1,17 @@
+import 'package:ForDev/ui/pages/login/login_presenter.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class LoginButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final presenter = Provider.of<LoginPresenter>(context);
+    return StreamBuilder<bool>(
+      stream: presenter.isFormValidStream,
+      builder: (context, snapshot) => RaisedButton(
+        onPressed: snapshot.data == true ? presenter.auth : null,
+        child: Text('Entrar'.toUpperCase()),
+      ),
+    );
+  }
+}
