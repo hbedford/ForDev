698b9e31a2923b3d75a6b0d4a692a478e0fab274
hugo bedford <hugo.bedford@gmail.com>
2021-03-02 19:00:04 -0300

    ensure EmailValidation return null if email is invalid

--- test/validation/validators/email_validation_test.dart
+++ test/validation/validators/email_validation_test.dart
@@ -5,7 +5,10 @@ class EmailValidation implements FieldValidation {
   final String field;
   EmailValidation(this.field);
   String validate(String value) {
-    return null;
+    final regex = RegExp(
+        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+    final isValid = value?.isNotEmpty != true || regex.hasMatch(value);
+    return isValid ? null : 'Campo inválido';
   }
 }
 
@@ -22,5 +25,8 @@ void main() {
   });
   test('Should return null if email is valid', () {
     expect(sut.validate('hugo.bedford@gmail.com'), null);
+  });
+  test('Should return null if email is invalid', () {
+    expect(sut.validate('hugo.bedford'), 'Campo inválido');
   });
 }
